a29a73a4e5a88374a485bd6f741fc7ae6ae6b538
Tosif145 <tklearning2000@gmail.com>
2024-07-09 09:45:08 +0530

    extra div remove

--- src/Components/pages/Contact.jsx
+++ src/Components/pages/Contact.jsx
@@ -22,64 +22,56 @@ const Contact = () => {
             <p>Contacts</p>
           </div>
           <div className="contact-info">
-
-              <div className="forms-container">
-                <div className="form-control signin-form">
-                  <form action="#">
-                    <h2>Send a message</h2>
-                    <input type="text" placeholder="First Name" required />
-                    <input type="text" placeholder="Last Name" required />
-                    <input type="email" placeholder="Email" required />
-                    <input type="number" placeholder="Phone number" />
-                    <button>Send</button>
-                  </form>
-                </div>
+              <div className="form-control signin-form">
+                <form action="#">
+                  <h2>Send a message</h2>
+                  <input type="text" placeholder="First Name" required />
+                  <input type="text" placeholder="Last Name" required />
+                  <input type="email" placeholder="Email" required />
+                  <input type="number" placeholder="Phone number" />
+                  <button>Send</button>
+                </form>
               </div>
-              <div className="intros-container">
-                <div className="intro-control signup-intro">
-                  <div className="contact-left">
-                    <h2>Contact Info</h2>
-                    <ul>
-                      <li>
-                        <span>
-                          <img src={direction} alt="" />
-                        </span>
-                        <span>
-                          Beside Barshal Water Tank, Manpur,<br /> Barhanti West
-                          Bengal 723156
-                        </span>
-                      </li>
+            <div className="contact-left">
+              <h2>Contact Info</h2>
+              <ul>
+                <li>
+                  <span>
+                    <img src={direction} alt="" />
+                  </span>
+                  <span>
+                    Beside Barshal Water Tank, Manpur,
+                    <br /> Barhanti West Bengal 723156
+                  </span>
+                </li>
 
-                      <li>
-                        <span>
-                          <img src={email} alt="" /> 
-                        </span>
-                        <span>
-                          <a href="mailto: kkghosh0099@gmail.com">
-                            kkghosh0099@gmail.com
-                          </a>
-                        </span>
-                      </li>
-                      <li>
-                        <span>
-                          <img src={call} alt="" /> 
-                        </span>
-                        <span>
-                          <a href="tel:+919007062180"> +91 9007062180 </a>
-                        </span>
-                      </li>
-                    </ul>
+                <li>
+                  <span>
+                    <img src={email} alt="" />
+                  </span>
+                  <span>
+                    <a href="mailto: kkghosh0099@gmail.com">
+                      kkghosh0099@gmail.com
+                    </a>
+                  </span>
+                </li>
+                <li>
+                  <span>
+                    <img src={call} alt="" />
+                  </span>
+                  <span>
+                    <a href="tel:+919007062180"> +91 9007062180 </a>
+                  </span>
+                </li>
+              </ul>
 
-                    <div className="social-icons">
-                      <img src={facebook} alt="" />
-                      <img src={instagram} alt="" />
-                      <img src={twitter} alt="" />
-                      <img src={linkedin} alt="" />
-                    </div>
-                  </div>
-                </div>
+              <div className="social-icons">
+                <img src={facebook} alt="" />
+                <img src={instagram} alt="" />
+                <img src={twitter} alt="" />
+                <img src={linkedin} alt="" />
               </div>
-  
+            </div>
           </div>
         </div>
         <div className="contact-bottom-div">
