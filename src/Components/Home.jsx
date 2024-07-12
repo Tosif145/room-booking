ddebd9ac4f2aa04bd4b7d6267894ea9af4180280
Tosif145 <tklearning2000@gmail.com>
2024-07-12 15:37:16 +0530

    content css update for 950px

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -6,7 +6,7 @@ import Services from './pages/Services';
 import Rooms from './pages/Rooms';
 import Contact from './pages/Contact';
 import Gallery from './pages/Gallery';
-
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 const Home = () => {
   const [selectedComponent, setSelectedComponent] = useState('home');
@@ -18,18 +18,20 @@ const Home = () => {
       case 'rooms':
         return <Rooms />;
       case 'gallery':
-        return <Gallery />
+        return <Gallery />;
       default:
-        return <Content  setSelectedComponent={setSelectedComponent}/>;
+        return <Content setSelectedComponent={setSelectedComponent} />;
     }
   };
- 
 
   return (
     <div className='container'>
-      <NavBar selectedComponent={selectedComponent}  setSelectedComponent={setSelectedComponent} />
-      {renderComponent()}
-     
+      <NavBar selectedComponent={selectedComponent} setSelectedComponent={setSelectedComponent} />
+      <TransitionGroup>
+        <CSSTransition key={selectedComponent} timeout={300} classNames="zoom">
+          <div>{renderComponent()}</div>
+        </CSSTransition>
+      </TransitionGroup>
     </div>
   );
 };
